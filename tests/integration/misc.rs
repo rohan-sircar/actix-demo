4d1118eb36246d57547aa241e52278599c491f83
Rohan Sircar <rohansircar@tuta.io>
2021-05-06 12:51:58 +0530

    refactor it tests

---
+++ tests/integration/misc.rs
@@ -0,0 +1,21 @@
+use crate::common;
+use actix_demo::get_build_info;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use actix_web::dev::Service as _;
+    use actix_web::http::StatusCode;
+    use actix_web::test;
+
+    #[actix_rt::test]
+    async fn get_build_info_should_succeed() {
+        let req = test::TestRequest::get().uri("/api/build-info").to_request();
+        let resp = common::test_app().await.call(req).await.unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+        let body: build_info::BuildInfo = test::read_body_json(resp).await;
+        assert_eq!(body, *get_build_info());
+        log::debug!("{:?}", body);
+    }
+}
