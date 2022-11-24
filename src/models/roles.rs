2374daa1033942207859caf08ed2e6a092891b81
Rohan Sircar <rohansircar@protonmail.com>
2022-11-24 15:05:16 +0530

    add newtype for roleid

--- src/models/roles.rs
+++ src/models/roles.rs
@@ -1,4 +1,7 @@
+use std::str::FromStr;
+
 use crate::schema::roles;
+use derive_more::{Display, Into};
 use diesel_derive_enum::DbEnum;
 use serde::{Deserialize, Serialize};
 
@@ -15,6 +18,54 @@ pub enum RoleEnum {
 #[derive(Debug, Clone, Deserialize, Serialize, Queryable, Identifiable)]
 #[table_name = "roles"]
 pub struct Role {
-    pub id: i32,
+    pub id: RoleId,
     pub name: RoleEnum,
+}
+
+#[derive(
+    Debug,
+    Clone,
+    Eq,
+    Hash,
+    PartialEq,
+    Deserialize,
+    Display,
+    Into,
+    Serialize,
+    DieselNewType,
+)]
+#[serde(try_from = "u32", into = "u32")]
+pub struct RoleId(pub i32);
+impl From<RoleId> for u32 {
+    fn from(s: RoleId) -> u32 {
+        //this should be safe to unwrap since our newtype
+        //does not allow negative values
+        s.0.try_into().unwrap()
+    }
+}
+
+impl FromStr for RoleId {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Ok(num) = s.parse::<u32>() {
+            num.try_into()
+                .map_err(|err| {
+                    format!("negative values are not allowed: {}", err)
+                })
+                .map(RoleId)
+        } else {
+            Err("expected unsigned int, received string".to_owned())
+        }
+    }
+}
+
+impl TryFrom<u32> for RoleId {
+    type Error = String;
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        value
+            .try_into()
+            .map_err(|err| format!("error while converting user_id: {}", err))
+            .map(RoleId)
+    }
 }
