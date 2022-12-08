c2b5877fbc200cac3f8d87b7a1216c03c91806fc
Rohan Sircar <rohansircar@protonmail.com>
2022-12-08 11:16:56 +0530

    use plain iterator in chan reader

--- src/utils/redis_channel_reader.rs
+++ src/utils/redis_channel_reader.rs
@@ -1,6 +1,5 @@
 use std::sync::Arc;
 
-use futures::prelude::*;
 use redis::{
     aio::ConnectionManager,
     streams::{StreamId, StreamReadOptions, StreamReadReply},
@@ -22,18 +21,6 @@ impl RedisChannelReader {
         &self.channel_name
     }
 
-    // #[async_recursion]
-    // async fn subscribe_loop(
-    //     mut conn: MultiplexedConnection,
-    //     channel_name: &str,
-    //     opts: &StreamReadOptions,
-    // ) -> Result<Vec<StreamId>, DomainError> {
-    //     let rep: StreamReadReply =
-    //         conn.xread_options(&[channel_name], &["$"], opts).await?;
-    //     let items = rep.keys.iter().flat_map(|x| x.ids.clone()).collect();
-    //     Ok(items)
-    // }
-
     pub async fn get_items(&self) -> Result<Vec<StreamId>, DomainError> {
         let mut conn = self.conn.clone();
         let opts = StreamReadOptions::default().block(0).count(5);
@@ -46,68 +33,15 @@ impl RedisChannelReader {
             .xread_options(&[&self.channel_name], &[&id], &opts)
             .await?;
         let _ = tracing::debug!("Received keys {:?}", &rep.keys);
-        let items = stream::iter(rep.keys.into_iter())
-            .flat_map(|x| stream::iter(x.ids.into_iter()))
-            .then(|x| async move {
-                {
-                    let mut lmi = self.last_msg_id.write().await;
-                    *lmi = Some(x.id.clone());
-                }
-                x
-            })
-            .collect()
-            .await;
+        let items = rep
+            .keys
+            .into_iter()
+            .flat_map(|x| x.ids.into_iter())
+            .collect::<Vec<StreamId>>();
+        let _ = if let Some(x) = items.last() {
+            let mut lmi = self.last_msg_id.write().await;
+            *lmi = Some(x.id.clone());
+        };
         Ok(items)
     }
 }
-
-// #[async_recursion]
-// async fn subscribe_loop2(
-//     mut conn: MultiplexedConnection,
-//     channel_name: &str,
-//     opts: &StreamReadOptions,
-// ) -> impl Stream<Item = StreamId> {
-//     let rep: StreamReadReply =
-//         conn.xread_options(&[channel_name], &["$"], opts).await?;
-//     let items: Vec<StreamId> = rep
-//         .keys
-//         .into_iter()
-//         .flat_map(|x| x.ids.into_iter())
-//         .collect();
-//     if !items.is_empty() {
-//         let keystream = stream::select(
-//             stream::iter(items),
-//             subscribe_loop2(conn, channel_name, opts),
-//         );
-//         keystream
-//     } else {
-//         stream::empty()
-//     }
-//     // Ok(items)
-// }
-
-// #[async_recursion]
-// pub async fn get_items_loop(
-//     mut conn: ConnectionManager,
-//     channel_name: &str,
-//     mut last_msg_id: String,
-// ) -> Result<Vec<StreamId>, DomainError> {
-//     let opts = StreamReadOptions::default().block(0);
-//     let rep: StreamReadReply = conn
-//         .xread_options(&[channel_name], &[&last_msg_id], &opts)
-//         .await?;
-//     let items: Vec<StreamId> = rep
-//         .keys
-//         .into_iter()
-//         .inspect(|x| {
-//             last_msg_id = x.key.clone();
-//         })
-//         .flat_map(|x| x.ids)
-//         .collect();
-//     if items.is_empty() {
-//         Ok(items)
-//     } else {
-//         Ok(get_items_loop(conn, channel_name, last_msg_id).await?)
-//     }
-//     // Ok(items)
-// }
