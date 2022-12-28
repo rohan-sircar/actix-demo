29161a597aa6cbf8cb40b37bff87570b157f9fa5
Rohan Sircar <rohansircar@protonmail.com>
2022-12-28 14:00:58 +0530

    adjust it-test script for github ci

--- tests/integration/it-test.py
+++ tests/integration/it-test.py
@@ -7,7 +7,7 @@ workdir = "tests/integration"
 
 os.system(f"docker-compose -f {workdir}/docker-compose.it-test.yml up -d ")
 while (True):
-    proc = subprocess.run(["docker", "exec", "-it", "actix-demo-test-postgres", "pg_isready"],
+    proc = subprocess.run(["docker", "exec", "-t", "actix-demo-test-postgres", "pg_isready"],
                           stdout=subprocess.PIPE)
     if proc.stdout.decode('utf-8').endswith("accepting connections\r\n"):
         break
